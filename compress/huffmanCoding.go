553b9e20546217e47206a6f5a021a325867141c5
OakAnderson <andersonfelipe01@live.com>
2020-04-25 01:49:29 -0300

    #4 - create huffmanCoding module This file contains imported functions that encode and decode a sequence of bytes

---
+++ compress/huffmanCoding.go
@@ -0,0 +1,60 @@
+package compress
+
+/*
+HuffmanEncode compresses the passed data with the Huffman Coding algorithm. It
+returns a byte slice with encoded data and the table of in codes.
+example:
+
+compress.HuffmanEncode([]byte("AAAABBBCCD")) returns:
+
+[ '1' '1' '1' '1' '0' '0' '0' '0' '0' '0'
+	'0' '1' '0' '0' '1' '0' '0' '1' '1' ]
+map['A': "1", 'B': "00", 'C': "010", 'D', "011"]
+
+The map can be used for decode the encoded byte sequence
+*/
+func HuffmanEncode(data []byte) ([]byte, map[byte]string) {
+	freqs := symbolsFrequency(data)
+	codeTable := encode(freqs)
+
+	var encoded []byte
+	for _, b := range data {
+		encoded = append(encoded, []byte(codeTable[b])...)
+	}
+	return encoded, codeTable
+}
+
+// HuffmanDecode decodes an encoded string by HuffmanEncode function.
+//
+// It receives a byte sequence and a map[byte]string code table. This
+// parameters is returned by HuffmanEncode function. It also returns a byte
+// sequence for the passed encoded byte sequence
+func HuffmanDecode(data []byte, codeTable map[byte]string) []byte {
+	var decoded []byte
+	reversedTable := reverseCode(codeTable)
+
+	var word string
+	for _, v := range data {
+		word += string(v)
+		if b, ok := reversedTable[word]; ok {
+			decoded = append(decoded, b)
+			word = ""
+		}
+	}
+	return decoded
+}
+
+// symbolsFrequency is a function that returns a slice of frequencies. That
+// means that this function counts every symbol in passed data and return a
+// []node slice with the symbol byte and counted frequency
+func symbolsFrequency(data []byte) (freqs frequencies) {
+	for _, b := range data {
+		if ok, indx := freqs.contains(b); ok {
+			freqs[indx].freq++
+		} else {
+			freqs = append(freqs, node{b: b, freq: 1, left: nil, right: nil})
+		}
+	}
+	freqs.sort()
+	return
+}
